9392264b91964a488bc9ed266938b57f6a7b8701
Kory Fong <kory.fong@rbc.com>
2016-07-05 13:36:54 -0400

    Updating manage.py with templated data

--- manage.py
+++ manage.py
@@ -3,8 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ace.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MODULE.settings")
     # Read port selected by the cloud for our application
+    # VCAP_APP_PORT is also PORT in Bluemix Public
     PORT = '0.0.0.0:' + os.getenv('VCAP_APP_PORT', '8000')
     cmd = sys.argv
 
